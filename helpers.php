1e888612b5e23c540d819004e9fa4a03a6812ddb
Jeroen Derks <info@magentron.com>
2018-06-28 13:21:00 +0200

    fixing SensioLabsInsight issues

--- helpers.php
+++ helpers.php
@@ -3,12 +3,6 @@
  * @author  Rizart Dokollari <r.dokollari@gmail.com>
  * @since   12/24/17
  */
-function dd($variable)
-{
-    var_dump($variable);
-    exit;
-}
-
 function subject($functionName)
 {
     return sprintf('ElasticEmail Test: %s', $functionName);
